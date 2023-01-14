8c95be1f02bfe8643d8049449ad732cfadac09bd
LFrakie <randymas15@hotmail.com>
2023-01-14 11:13:12 -0500

    start project /gitu-2

---
+++ main.js
@@ -0,0 +1,15 @@
+navigator.mediaDevices.
+  getUserMedia({ audio: false, video: true })
+  .then((stream) => {
+    console.log(stream)
+
+    let video = document.getElementById('video')
+
+    video.srcObject = stream
+
+    })
+  .catch((err) => console.log(err));
+
+// Font: https://www.youtube.com/watch?v=k8h4Y-61FMk&ab_channel=ProCodeTv
+
+
