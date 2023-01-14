9136ae61c1778b1ddeb6120eb6304d13bb56c3e9
LFrakie <randymas15@hotmail.com>
2023-01-14 11:34:31 -0500

    update /gitu-2

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+// navigator.mediaDevices.
+//   getUserMedia({ audio: false, video: true })
+//   .then((stream) => {
+//     console.log(stream)
+
+//     let video = document.getElementById('video')
+
+//     video.srcObject = stream
+
+//     })
+//   .catch((err) => console.log(err));
+
+// Font: https://www.youtube.com/watch?v=k8h4Y-61FMk&ab_channel=ProCodeTv
+
+function camActive(){
 navigator.mediaDevices.
   getUserMedia({ audio: false, video: true })
   .then((stream) => {
@@ -9,7 +24,4 @@ navigator.mediaDevices.
 
     })
   .catch((err) => console.log(err));
-
-// Font: https://www.youtube.com/watch?v=k8h4Y-61FMk&ab_channel=ProCodeTv
-
-
+}
